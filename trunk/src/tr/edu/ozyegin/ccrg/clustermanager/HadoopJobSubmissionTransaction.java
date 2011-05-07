40b66a0cbe1c89f35a0545a0263cc04487a4eae2
becomen@gmail.com <becomen@gmail.com@a53f6123-425c-c01c-45f6-96d4c722dfc6>
2011-05-07 13:51:24 +0000

    HadoopJobSubmissionTransaction algorithm completed

---
+++ trunk/src/tr/edu/ozyegin/ccrg/clustermanager/HadoopJobSubmissionTransaction.java
@@ -0,0 +1,35 @@
+package tr.edu.ozyegin.ccrg.clustermanager;
+public class HadoopJobSubmissionTransaction extends JobSubmissionTransaction {
+
+	private String jarDir = "";
+	private int mapperCount = 0;
+	private int reducerCount = 0;
+	private String dfsInputDir = "";
+	private String dfsOutputDir = "";
+
+	public HadoopJobSubmissionTransaction(final String string, final String jarDir, final String dfsInputDir, final String dfsOutputDir, final int mapperCount, final int reducerCount ) {
+		super(string);
+		this.jarDir = jarDir;
+		this.dfsInputDir = dfsInputDir;
+		this.dfsOutputDir = dfsOutputDir;
+		this.mapperCount = mapperCount;
+		this.reducerCount = reducerCount;
+	}
+
+	
+	public String getJarDir() {
+		return this.jarDir;
+	}
+	public int getMapperCount() {
+		return this.mapperCount;
+	}
+	public int getReducercount(){
+	  return this.reducerCount;
+	}
+	public String getDfsInputDir(){
+	  return this.dfsInputDir;
+	}
+	public String getDfsOutputDir(){
+	  return this.dfsOutputDir;
+	}
+}
