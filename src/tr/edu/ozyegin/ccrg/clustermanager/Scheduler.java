759aedaf89691af40f812b5df003ca23a1cc17c4
becomen <becomen@gmail.com>
2011-05-10 22:25:35 +0000

    clusterstatus and cluster added to the scheduler base class

--- src/tr/edu/ozyegin/ccrg/clustermanager/Scheduler.java
+++ src/tr/edu/ozyegin/ccrg/clustermanager/Scheduler.java
@@ -1,6 +1,9 @@
 package tr.edu.ozyegin.ccrg.clustermanager;
+import tr.edu.ozyegin.ccrg.clustermanager.cluster.Cluster;
+import tr.edu.ozyegin.ccrg.clustermanager.states.ClusterStatus;
 public interface Scheduler {
-	
+	Cluster cluster = new Cluster();
+	ClusterStatus status = new ClusterStatus();
 	void ScheduleAlgorithm(Job j);
 	
 	void update();
